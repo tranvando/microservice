23391b74f4d8ffb1a7806c481bab35ced8c11bb1
tranvando <dotv@itsol.vn>
2022-09-21 18:00:59 +0700

    up

--- discoveryserver/src/main/java/com/example/discoveryserver/DiscoveryserverApplication.java
+++ discoveryserver/src/main/java/com/example/discoveryserver/DiscoveryserverApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DiscoveryserverApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(DiscoveryserverApplication.class, args);
     }
 
